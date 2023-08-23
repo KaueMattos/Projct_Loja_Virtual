d98864a9edcef5ee49bd632353f0d4cb34f66819
Kaue Mattos <125100288+KaueMattos@users.noreply.github.com>
2023-08-23 15:42:26 -0300

    AdicionandoCoisasNovas

--- index.php
+++ index.php
@@ -3,16 +3,51 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minha Loja</title>
+    <title>Flower Harbor</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+        .navbar{
+            margin-bottom: 0;
+        }
+    </style>
 </head>
 <body>
 <?php 
 include 'nav.php'
 ?>
+<?php 
+include 'cabecario.html'?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-3">
+        <img src="https://placehold.it/450x320" class="img-responsive" style="width:100%;" alt="">
+        <div><h1>Nome do Produto</h1></div>
+        <div><h4>Nome do R$158,00</h4></div>
+        </div>
+
+        <div class="col-sm-3">
+        <img src="https://placehold.it/450x320" class="img-responsive" style="width:100%;" alt="">
+        <div><h1>Nome do Produto</h1></div>
+        <div><h4>Nome do R$158,00</h4></div>
+        </div>
+
+        <div class="col-sm-3">
+        <img src="https://placehold.it/450x320" class="img-responsive" style="width:100%;" alt="">
+        <div><h1>Nome do Produto</h1></div>
+        <div><h4>Nome do R$158,00</h4></div>
+        </div>
+
+        <div class="col-sm-3">
+        <img src="https://placehold.it/450x320" class="img-responsive" style="width:100%;" alt="">
+        <div><h1>Nome do Produto</h1></div>
+        <div><h4>Nome do R$158,00</h4></div>
+        </div>
+    </div>
+</div>
+<?php include 'rodape.html';?>
 </body>
 </html>
